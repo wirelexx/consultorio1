bdf5f185ca71ee764146d1d77dde081df2c9aa48
ale <wirelexx@gmail.com>
2020-12-01 20:47:24 -0300

    ultimo

--- gestion_consultorio/static/js/script.js
+++ gestion_consultorio/static/js/script.js
@@ -1,6 +1,8 @@
 
 window.onload=function() {
 
+
+//alerta al eliminar
 (function() {
   const btnDelete = document.querySelectorAll('.btn-delete');
   const btnArray = Array.from(btnDelete);
@@ -12,4 +14,36 @@ window.onload=function() {
     }); 
   });
 }());
-}
+
+// busqueda en tabla
+    $(document).ready(function(){
+      $("#buscar_tabla").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#tabla_busqueda tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });
+
+    function editar_paciente(boton){
+      var str = boton.value;
+      var res = str.split(",");
+      document.getElementById("input_id_paciente_editar").value=res[0];
+      document.getElementById("input_nombre_paciente").value=res[1];
+      document.getElementById("input_apellido_paciente").value=res[2];
+    }
+    function mostrarvalor(boton){
+      var str = boton.value;
+      var res = str.split(",");
+      document.getElementById("input_id_paciente_nuevo_turno").value=res[0];
+      document.getElementById("nombre_paciente").innerHTML="Nuevo turno Paciente: "+res[1]+" "+res[2];
+      
+      var id_medico = document.getElementById("tabla_medico");
+      document.getElementById("input_id_medico").value=id_medico.value;
+    }
+
+    function select_medico(){
+      var id_medico = document.getElementById("tabla_medico");
+      document.getElementById("input_id_medico").value=id_medico.value;
+    }
+  }
